5c4043251548aef9224c8dcfff4d82f9d3226f76
Morten Bartvig <bartvig@gmail.com>
2017-08-31 17:08:51 +0200

    Added exception constants.

--- src/SimpleGAException.php
+++ src/SimpleGAException.php
@@ -14,6 +14,8 @@ use Throwable;
 define('SIMPLEGA_NO_FITNESS', 1);
 define('SIMPLEGA_NO_GENOME_FOUND', 2);
 define('SIMPLEGA_TOO_MANY_GENES', 3);
+define('SIMPLEGA_WRONG_GENE_VALUE', 4);
+define('SIMPLEGA_GENE_NOT_EXISTS', 5);
 
 class SimpleGAException extends \Exception {
   public function __construct(
