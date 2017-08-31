a4eb72388cf90bb16606c4b2b32426ab5ad2a91a
Morten Bartvig <bartvig@gmail.com>
2017-08-31 16:51:55 +0200

    Moved getPart functionality to SimpleGA and added comments.

--- src/Genome.php
+++ src/Genome.php
@@ -1,30 +1,62 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: morten
- * Date: 8/31/17
- * Time: 9:58 AM
+ * @file
+ * Genome class.
  */
 
 namespace SimpleGA;
 
-
+/**
+ * Class Genome
+ *
+ * Class for handling a single genome.
+ *
+ * @package SimpleGA
+ */
 class Genome implements GenomeInterface {
 
+  /**
+   * @var null
+   *   Random number generator.
+   */
   protected $randomGenerator = NULL;
 
+  /**
+   * @var null
+   *   Fitness of genome.
+   */
   protected $fitness = NULL;
 
+  /**
+   * @var array
+   *   Genes as an array.
+   */
   protected $genome = [];
 
+  /**
+   * Genome constructor.
+   *
+   * @param $randomGenerator
+   *   Random number generator to be used for selecting genes.
+   */
   public function __construct($randomGenerator) {
     $this->randomGenerator = $randomGenerator;
   }
 
+  /**
+   * Mutate genome.
+   *
+   * Override this method.
+   */
   public function mutate() {
-    // TODO: Implement mutate() method.
   }
 
+  /**
+   * Generate genome by gene parts.
+   *
+   * @param array $parts
+   *   One or more gene parts.
+   */
   public function generate(array $parts = []) {
     if (!empty($parts)) {
       foreach ($parts as $part) {
@@ -33,9 +65,23 @@ class Genome implements GenomeInterface {
     }
   }
 
+  /**
+   * Evaluate genome.
+   *
+   * Override this method.
+   */
   public function evaluate() {
   }
 
+  /**
+   * Get fitness of genome, if it's evaluated.
+   *
+   * @return int
+   *   Return fitness.
+   *
+   * @throws \SimpleGA\SimpleGAException
+   *   Throw exception if fitness hasn't been evaluated.
+   */
   public function getFitness() {
     if (is_null($this->fitness)) {
       throw new SimpleGAException('Genome is not evaluated', SIMPLEGA_NO_FITNESS);
@@ -44,9 +90,33 @@ class Genome implements GenomeInterface {
     return $this->fitness;
   }
 
+  /**
+   * Get part of genome.
+   *
+   * Override this method.
+   *
+   * @param int $a
+   *   First point to split.
+   *
+   * @param int $b
+   *   Second point to split.
+   *
+   * @return array
+   *   Return part of genome.
+   */
   public function getPart($a, $b) {
+    $genome = $this->genome;
+    $result = array_slice($genome, $a - 1, $b - $a + 1);
+    return $result;
   }
 
+  /**
+   * Return genome as a string.
+   *
+   * Override this method.
+   *
+   * @return string
+   */
   public function toString() {
     return '';
   }
